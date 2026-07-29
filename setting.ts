37a2fa7faa9d5a92407b53962ddf34a77ee0ae38
kanami <k.fukuoka@keio.jp>
2026-07-29 13:31:11 -0400

    setup ogp

--- setting.ts
+++ setting.ts
@@ -1,2 +1,2 @@
 // ex) export const TARGET_URL = "https://xxxx.github.io/tech-blog.github.io/";
-export const TARGET_URL = "https://kanamin05.github.io/tech-blog.github.io/top.html";
+export const TARGET_URL = "https://kanamin05.github.io/tech-blog.github.io/";
